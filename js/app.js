991db9eb864a4e1c0f27a3f71f7b1aeaf9c36a90
Your Name <ibrahimhossain3134@gamil.com>
2021-09-04 16:56:48 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const apiKey = `17f58653d63c3f12ca3fa094db75df7f`
+const searchtemp = () => {
+    const city = document.getElementById('city-name')
+    const cityText = city.value
+    city.value = ''
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityText}&appid=${apiKey}&units=metric`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemparature(data))
+}
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text
+}
+const displayTemparature = temparature => {
+    setInnerText('city', temparature.name)
+    setInnerText('temperature', temparature.main.temp)
+    setInnerText('sky-condition', temparature.weather[0].main)
+    const url = `http://openweathermap.org/img/wn/${temparature.weather[0].icon}@2x.png`
+    const images = document.getElementById('icons')
+    images.setAttribute('src', url)
+}
